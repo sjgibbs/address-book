5a9fe2dae4630b7a0d3563802a8cff25df6b52ef
Simon Gibbs <simon.gibbs@ft.com>
2016-03-23 18:05:32 +0000

    Add formatted question 2 output Split large method

--- src/main/java/com/github/sjgbbs/gumtree/AddressBookApplication.java
+++ src/main/java/com/github/sjgbbs/gumtree/AddressBookApplication.java
@@ -12,7 +12,6 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.FileNotFoundException;
-import java.time.LocalDate;
 import java.util.Set;
 
 
@@ -27,29 +26,29 @@ public class AddressBookApplication {
 		File file = new File("AddressBook");
 		System.out.println(file.getName() + " file exists = " + file.exists());
 
-		Set<Person> people = null;
 		BufferedReader bufferedReader = null;
 
 		try {
 			bufferedReader = new BufferedReader(new FileReader(file));
-			people = reader.readAll(bufferedReader, false);
+      Set<Person> people = reader.readAll(bufferedReader, false);
 
 			System.out.printf("loaded %d people", people.size());
 			System.out.println();
 
+      // 1. How many males
+
 			Demographics demographics = new Demographics(people);
 
-			System.out.printf("%d were male", demographics.genderHistogram().get(GeneticGender.MALE));
-			System.out.println();
+      reportNumberOfMales(demographics);
+      System.out.println();
 
-			Person bill = findPerson(people,"Bill McKnight");
-			Person paul = findPerson(people,"Paul Robinson");
+      // 2. oldest person
+      reportOldestPerson(demographics);
+      System.out.println();
 
-			// 3. How many days older is X than Y?
-      long difference = paul.ageDifferenceInDays(bill);
-			System.out.printf("%s is %d days older than %s", bill.getFullName(), difference, paul.getFullName());
-			System.out.println();
-      System.out.printf("(about %d years)", (int) Math.round(difference / 365.25));
+      // 3. How many days older is X than Y?
+
+      reportAgeDifferenceOfArbitraryPair(people);
       System.out.println();
 
 
@@ -72,7 +71,26 @@ public class AddressBookApplication {
 
 	}
 
-	private static Person findPerson(Set<Person> people, String fullName) {
+  private static void reportAgeDifferenceOfArbitraryPair(Set<Person> people) {
+    Person bill = findPerson(people,"Bill McKnight");
+    Person paul = findPerson(people,"Paul Robinson");
+
+    long difference = paul.ageDifferenceInDays(bill);
+    System.out.printf("%s is %d days older than %s", bill.getFullName(), difference, paul.getFullName());
+    System.out.println();
+    System.out.printf("(about %d years)", (int) Math.round(difference / 365.25));
+  }
+
+  private static void reportOldestPerson(Demographics demographics) {
+    Person oldestPerson = demographics.findOldestIndividual();
+    System.out.printf("The oldest person is: %s", oldestPerson.getFullName());
+  }
+
+  private static void reportNumberOfMales(Demographics demographics) {
+    System.out.printf("%d were male", demographics.genderHistogram().get(GeneticGender.MALE));
+  }
+
+  private static Person findPerson(Set<Person> people, String fullName) {
 		for(Person person : people) {
 			if(person.getFullName().equals(fullName)) {
 				return person;
