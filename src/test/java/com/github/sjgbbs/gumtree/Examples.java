e0b77064b570c3268a89ff19bd7b8d1966d43165
Simon Gibbs <simon.gibbs@ft.com>
2016-03-23 16:48:50 +0000

    Import examples and synchronise the line format

---
+++ src/test/java/com/github/sjgbbs/gumtree/Examples.java
@@ -0,0 +1,16 @@
+package com.github.sjgbbs.gumtree;
+
+/**
+ * Examples
+ *
+ * @author Simon
+ */
+public class Examples {
+
+	public static final String JEFF = "Jeff Briton, male, 16/03/77";
+	public static final String TOM = "Tom Soyer, male, 15/04/77";
+	public static final String SAV = "Sav Purell, male, 22/09/78";
+	public static final String LIAM = "Liam Bolt, male, 22/02/80";
+	public static final String SUZIE = "Suzie Long, female, 17/08/81";
+
+}
